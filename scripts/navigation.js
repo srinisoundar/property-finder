ade8f7f499a97ac81f5fd54e4c28fc0282d353e9
srini <srini.cug@gmail.com>
2016-08-15 02:02:39 +0200

    navigation

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -3,9 +3,21 @@ import template from '../templates/navigation.html';
 
 const navFragment = utils.convertToDomElement(template);
 
-navFragment.addEventListener('click', () => {
-    console.log('click');
-});
+const menu = navFragment.querySelector('.navigation-mobile__menu');
+const menu_right = navFragment.querySelector('.navigation-mobile__menu-right');
+const icon = navFragment.querySelector('.navigation__icon-menu');
+
+const elements = navFragment.querySelectorAll('.navigation__icon, .navigation-mobile__menu-left,.navigation-mobile__menu-right');
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', () => {
+        menu.classList.toggle('navigation-mobile__menu--transform');
+        menu_right.classList.toggle('navigation-mobile__menu-right--transform');
+        icon.classList.toggle('fa-bars');
+        icon.classList.toggle('fa-times');
+
+    });
+}
+
 
 
 export default navFragment;
